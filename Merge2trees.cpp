54e6a67d5bcd9f90fcbe3fbfdefd0df73a534bae
Deshan SANDANAYAKE <pdeshans@sbgat406.in2p3.fr>
2022-02-08 11:04:56 +0100

    Now enter file names on the go!

--- Merge2trees.cpp
+++ Merge2trees.cpp
@@ -14,13 +14,24 @@ using namespace std;
 int Merge2trees()
 {
     TList *list     = new TList();
-    
-    TFile *infile1  = new TFile("TB_Scurve_ThirdPE_pars.root", "READ");
-    TTree *tree1    = (TTree*)infile1->Get("TB_Scurve_par");
+    TString file1, file2, tr1, tr2;
+
+    cout<<"Input the ROOT file no. 1"<<endl;
+    cin>>file1;
+    cout<<"Input the corresponding TTree name"<<endl;
+    cin>>tr1;
+
+    cout<<"Input the ROOT file no. 2"<<endl;
+    cin>>file2;
+    cout<<"Input the corresponding TTree name"<<endl;
+    cin>>tr2;
+
+    TFile *infile1  = new TFile(file1, "READ");
+    TTree *tree1    = (TTree*)infile1->Get(tr1);
     list->Add(tree1);
     
-    TFile *infile2  = new TFile("TB_Scurve_ThirdPE_pars2.root", "READ");  
-    TTree *tree2    = (TTree*)infile2->Get("TB_Scurve_par");
+    TFile *infile2  = new TFile(file2, "READ");  
+    TTree *tree2    = (TTree*)infile2->Get(tr2);
     list->Add(tree2);
 
     TFile *outfile  = new TFile("Scurve_ThirdPE_sum.root","recreate");
